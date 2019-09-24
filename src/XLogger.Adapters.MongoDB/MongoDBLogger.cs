ab4e1da29fc5abe843a5dd2d79227d7f99ece2ce
André Secco <andreluizsecco@yahoo.com.br>
2019-09-24 16:27:40 -0300

    Add support for .NET Core 3.0 and Update MongoDB Driver

--- src/XLogger.Adapters.MongoDB/MongoDBLogger.cs
+++ src/XLogger.Adapters.MongoDB/MongoDBLogger.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using XLogger.Adapters.MongoDB.Models;
